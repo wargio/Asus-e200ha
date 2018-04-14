83d0f25eeac7fa58e3c02a53d3cc6bb3f95d7283
wargio <wargio@libero.it>
2018-04-14 17:29:24 +0200

    several fixes

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ Asus E200HA Custom Kernel (4.12 & 4.13)
 
 clone this repo and run as user (not root) `build_e200ha-4.1x.sh`
 
+* Linux 4.12 `build_e200ha-4.12.sh`
+* Linux 4.13 `build_e200ha-4.13.sh`
+
+## Kernel Config
+
+* Enable `EXT3` and `EXT4` support under `filesystems`
--- build_e200ha-4.12.sh
+++ build_e200ha-4.12.sh
@@ -1,38 +1,61 @@
 #!/bin/bash
 MAINFLDR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+KVERSION="4.12"
+KFOLDER="sound-topic/asus-e100h-$KVERSION/"
 
-echo "checking requirements"
+echo ""
+echo "MAINFLDR: $MAINFLDR"
+echo "KVERSION: $KVERSION"
+echo "KFOLDER:  $KFOLDER"
+echo ""
+
+echo "Checking requirements"
 command -v acpid >/dev/null 2>&1 || sudo pacman -S acpid
 
-if [ ! -d "sound-topic/asus-e100h-4.12" ]; then
-	if [ ! -f "asus-e100h-4.12.tar.gz" ]; then
-		wget https://git.kernel.org/pub/scm/linux/kernel/git/tiwai/sound.git/snapshot/sound-topic/asus-e100h-4.12.tar.gz
+trap ctrl_c INT
+
+function ctrl_c() {
+	exit 1;
+}
+
+if [ ! -d "$KFOLDER" ]; then
+	if [ ! -f "asus-e100h-$KVERSION.tar.gz" ]; then
+		echo "Downloading Linux Kernel $KVERSION"
+		wget "https://git.kernel.org/pub/scm/linux/kernel/git/tiwai/sound.git/snapshot/$KFOLDER.tar.gz"
 	fi
-	tar xvf asus-e100h-4.12.tar.gz
-	cd sound-topic/asus-e100h-4.12/
+	echo "Unpacking Linux Kernel $KVERSION"
+	tar xf "asus-e100h-$KVERSION.tar.gz"
+	cd "$KFOLDER"
+	echo "Preparing folder"
 	make clean && make mrproper
-	cp config-e200ha sound-topic/asus-e100h-4.12/.config
+	cp "$MAINFLDR/config-e200ha" .config
 	make xconfig
 	make
 else
-	cd sound-topic/asus-e100h-4.12/
+	cd "$KFOLDER"
 fi
+echo "Installing modules"
 sudo make modules_install
+echo "Preparing image"
 sudo cp -v arch/x86_64/boot/bzImage /boot/vmlinuz-e200ha
 sudo cp "$MAINFLDR/mkinitcpio-e200ha.conf" "/etc/mkinitcpio-e200ha.conf"
 sudo cp "$MAINFLDR/e200ha.preset" "/etc/mkinitcpio.d/e200ha.preset"
-if [ ! -f "/etc/acpi/handler.sh.backup" ];
+echo "Patching handlers"
+if [ ! -f "/etc/acpi/handler.sh.backup" ]; then
 	sudo cp "/etc/acpi/handler.sh" "/etc/acpi/handler.sh.backup"
 fi
 sudo cp "$MAINFLDR/acpi-handler-e200ha.sh" "/etc/acpi/handler.sh"
-if [ ! -f "/etc/asound.conf.backup" ];
+echo "Patching audio"
+if [ ! -f "/etc/asound.conf.backup" ]; then
 	sudo cp "/etc/asound.conf" "/etc/asound.conf.backup"
 fi
 sudo cp "$MAINFLDR/asound-e200ha.conf" "/etc/asound.conf"
 if [ ! -d "/usr/share/alsa/ucm/chtcx2072x/" ]; then
 	sudo cp -r "$MAINFLDR/chtcx2072x/" "/usr/share/alsa/ucm/"
 fi
+echo "Building image"
 sudo mkinitcpio -p e200ha
+echo "Updating grub"
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 
 echo "you can now reboot (please boot e200ha linux kernel)"
